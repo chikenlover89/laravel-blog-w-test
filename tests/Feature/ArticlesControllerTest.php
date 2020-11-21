4cf99a119d06f0a94fd9646ca6520283f209280e
Aivars <aivars.silis@gmail.com>
2020-11-21 15:19:22 +0200

    tests

---
+++ tests/Feature/ArticlesControllerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ArticlesControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function testArticlesIndex():void
+    {
+        $this->followingRedirects();
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200);
+    }
+
+    public function testArticlesCreate():void
+    {
+        $this->followingRedirects();
+        $response = $this->get(route('articles.create'));
+
+        $response->assertStatus(200);
+    }
+
+    public function testArticlesStore():void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $this->followingRedirects();
+
+        $response = $this->post(route('articles.store'), [
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => 'Example title',
+            'content' => 'Example content'
+        ]);
+    }
+    
+    public function testArticlesDelete(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $article = Article::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => $article->title,
+            'content' => $article->content
+        ]);
+
+        $this->followingRedirects();
+
+        $response = $this->delete(route('articles.destroy', $article));
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('articles', [
+            'user_id' => $user->id,
+            'title' => $article->title,
+            'content' => $article->content
+        ]);
+    }
+}
